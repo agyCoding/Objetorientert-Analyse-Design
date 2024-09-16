63c8b3a793c65e818421c44ae2e5085f6f168462
agyCoding <agata.olaussen@gmail.com>
2024-09-16 10:04:49 +0200

    Initial commit. Creating project structure with JavaFX and MVC model

---
+++ src/main/java/com/oap2024team7/team7mediastreamingapp/App.java
@@ -0,0 +1,13 @@
+package com.oap2024team7.team7mediastreamingapp;
+
+
+public class App {
+
+	public static void main(String[] args) throws Exception {
+		new App().proceed();
+	}
+
+	public void proceed() {
+		System.out.println("Hello world!");
+	}
+}
