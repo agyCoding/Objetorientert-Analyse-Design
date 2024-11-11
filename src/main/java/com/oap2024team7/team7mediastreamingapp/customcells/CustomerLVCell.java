bea36ab25e62f890de021c409707227cf5126105
agyCoding <agata.olaussen@gmail.com>
2024-11-11 21:52:18 +0100

    Added new view and controller for the user management page (admin). It allows the admin to view users in a LV, filter it and save users to a local file

---
+++ src/main/java/com/oap2024team7/team7mediastreamingapp/customcells/CustomerLVCell.java
@@ -0,0 +1,19 @@
+package com.oap2024team7.team7mediastreamingapp.customcells;
+
+import com.oap2024team7.team7mediastreamingapp.models.Customer;
+
+import javafx.scene.control.ListCell;
+
+public class CustomerLVCell extends ListCell<Customer> {
+    @Override
+    protected void updateItem(Customer customer, boolean empty) {
+        super.updateItem(customer, empty);
+        if (empty || customer == null) {
+            setText(null);
+        } else {
+            // Display the first name, last name and email in the ListView
+            setText(customer.getFirstName() + " " + customer.getLastName() + " (" + customer.getEmail() + ")");
+        }
+    }
+    
+}
